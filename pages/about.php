ad4f8e61f896fbd77c8b2ead311553a61f538a0e
andualem mamo <andualem7676@gmail.com>
2022-06-04 15:58:58 +0300

    about

--- pages/about.php
+++ pages/about.php
@@ -1,3 +1,12 @@
 <?php include '../components/nav.php'?>
 
+<div class="container col-10">
+    <div class="py-5 col-6 sm-col-10">
+        <h1 class="mb-3">About as</h1>
+        <p> &nbsp; &nbsp; Our goal is to deliver a place where anyone photographer or not to come and
+            show there photos and advertize themself it also help users to find images they like download
+             and use it. for companys they can come and find best phptographer they like and hire them
+        </p>
+    </div>
+</div>
 <?php include '../components/footer.php'?>
